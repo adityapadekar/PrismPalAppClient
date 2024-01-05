fdea84dd2b070b499c43ad156f35049bccb410a5
Aditya <adityarpadekar@gmail.com>
2024-01-05 23:37:47 +0530

    netlify error fix 2

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_URL || "http://localhost:8080",
+    baseURL:
+        `${process.env.BACKEND_URL}/api/v1` || "http://localhost:8080/api/v1",
 });
 
 export default api;
